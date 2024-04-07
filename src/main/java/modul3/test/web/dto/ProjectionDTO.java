6e0d046923555f36fd81fe4769b23a56d636a688
Marko Smiljanic <smiljanicmarko992@gmail.com>
2024-04-07 20:30:52 +0200

    ProjectionDTO created

---
+++ src/main/java/modul3/test/web/dto/ProjectionDTO.java
@@ -0,0 +1,107 @@
+package modul3.test.web.dto;
+
+import java.time.LocalDateTime;
+
+public class ProjectionDTO {
+
+	private Long id;	
+	
+	private Long movieId;
+	private String movieName;
+	
+	private Long projectionTypeId;
+	private String projectionType;	
+	
+	private Long theaterId;
+	
+	private String theaterName;
+	
+	private LocalDateTime time;
+	
+	private Double price;	
+
+	private Long adminId;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getMovieId() {
+		return movieId;
+	}
+
+	public void setMovieId(Long movieId) {
+		this.movieId = movieId;
+	}
+
+	public String getMovieName() {
+		return movieName;
+	}
+
+	public void setMovieName(String movieName) {
+		this.movieName = movieName;
+	}
+
+	public Long getProjectionTypeId() {
+		return projectionTypeId;
+	}
+
+	public void setProjectionTypeId(Long projectionTypeId) {
+		this.projectionTypeId = projectionTypeId;
+	}
+
+	public String getProjectionType() {
+		return projectionType;
+	}
+
+	public void setProjectionType(String projectionType) {
+		this.projectionType = projectionType;
+	}
+
+	public Long getTheaterId() {
+		return theaterId;
+	}
+
+	public void setTheaterId(Long theaterId) {
+		this.theaterId = theaterId;
+	}
+
+	public String getTheaterName() {
+		return theaterName;
+	}
+
+	public void setTheaterName(String theaterName) {
+		this.theaterName = theaterName;
+	}
+
+	public LocalDateTime getTime() {
+		return time;
+	}
+
+	public void setTime(LocalDateTime time) {
+		this.time = time;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public Long getAdminId() {
+		return adminId;
+	}
+
+	public void setAdminId(Long adminId) {
+		this.adminId = adminId;
+	}
+	
+	
+	
+}
