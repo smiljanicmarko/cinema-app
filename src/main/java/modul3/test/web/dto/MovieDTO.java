8691db30f4610dae7a95bc2db3f4558085718da2
Marko Smiljanic <smiljanicmarko992@gmail.com>
2024-04-07 21:20:33 +0200

    MovieDTO created

---
+++ src/main/java/modul3/test/web/dto/MovieDTO.java
@@ -0,0 +1,107 @@
+package modul3.test.web.dto;
+
+public class MovieDTO {
+
+	private Long id;
+	
+	private String name;
+
+	private String director;
+	
+	private String actors;
+
+	private String genres;
+	
+	private Integer duration;
+	
+	private String distributor;
+	
+	private String country;
+	
+	private Integer year;
+
+	private String description;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDirector() {
+		return director;
+	}
+
+	public void setDirector(String director) {
+		this.director = director;
+	}
+
+	public String getActors() {
+		return actors;
+	}
+
+	public void setActors(String actors) {
+		this.actors = actors;
+	}
+
+	public String getGenres() {
+		return genres;
+	}
+
+	public void setGenres(String genres) {
+		this.genres = genres;
+	}
+
+	public Integer getDuration() {
+		return duration;
+	}
+
+	public void setDuration(Integer duration) {
+		this.duration = duration;
+	}
+
+	public String getDistributor() {
+		return distributor;
+	}
+
+	public void setDistributor(String distributor) {
+		this.distributor = distributor;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public Integer getYear() {
+		return year;
+	}
+
+	public void setYear(Integer year) {
+		this.year = year;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}  
+	
+	
+	
+}
