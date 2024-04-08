daa3617c843cbf7434d1708042071180ae8a94f2
Marko Smiljanic <smiljanicmarko992@gmail.com>
2024-04-09 00:27:52 +0200

    Movie details finished for now. Later add buy ticket button

--- Front/src/Entitet/OneMovie.js
+++ Front/src/Entitet/OneMovie.js
@@ -1,7 +1,7 @@
 import { useCallback, useEffect, useState } from "react"
 import { useNavigate, useParams } from "react-router-dom"
 import TestAxios from "../apis/TestAxios"
-
+import { Button,Card,  Col, Form, FormGroup, FormLabel, Row, Table } from 'react-bootstrap';
 const OneMovie = () => {
 
     const urlParams = useParams()
@@ -33,7 +33,7 @@ const OneMovie = () => {
     };
 
     return (
-        <div>    
+        <div>
 
             <div class="jumbotron jumbotron-fluid">
                 <div class="container">
@@ -41,6 +41,51 @@ const OneMovie = () => {
                     <p class="lead">Duration: {movie.duration} min. | Genre: {getGenresStringFromMap(movie.genres)}</p>
                 </div>
             </div>
+
+            <div>
+                <Row>
+                <Col>
+                <Table>
+                    <tr>
+                    <th>Director: </th> <td>{movie.director}</td>
+                    </tr>
+                    <tr>
+                    <th>Actors: </th> <td>{movie.actors}</td>
+                    </tr>
+                    <tr>
+                    <th>Genre: </th> <td>{getGenresStringFromMap(movie.genres)}</td>
+                    </tr>
+                    <tr>
+                    <th>Duration: </th> <td>{movie.duration}</td>
+                    </tr>
+                    <tr>
+                    <th>Distributor: </th> <td>{movie.distributor}</td>
+                    </tr>
+                    <tr>
+                    <th>Country: </th> <td>{movie.country}</td>
+                    </tr>
+                    <tr>
+                    <th>Year: </th> <td>{movie.year}</td>
+                    </tr>
+                </Table>
+                </Col>
+                <Col>
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Body>
+                            <Card.Title>{movie.name}</Card.Title>
+                            <Card.Subtitle className="mb-2 text-muted">Storyline</Card.Subtitle>
+                            <Card.Text>
+                               {movie.description}
+                            </Card.Text>
+                          {/*  <Card.Link href="#">Card link</Card.Link>  Change 'href' to 'to' if using React Router 
+                            <Card.Link href="#">Another link</Card.Link> {/* Change 'href' to 'to' if using React Router */}
+                        </Card.Body>
+                    </Card>
+                </Col>
+                </Row>
+
+            </div>
+
 
 
         </div>
