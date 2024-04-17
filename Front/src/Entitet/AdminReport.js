d0d6221e3871435db005434d283b9830bc2a16dd
Marko Smiljanic <smiljanicmarko992@gmail.com>
2024-04-17 23:17:56 +0200

    Report working properly

--- Front/src/Entitet/AdminReport.js
+++ Front/src/Entitet/AdminReport.js
@@ -20,74 +20,28 @@ const AdminReport = () => {
   const isAdmin = decoded?.role?.authority === "ROLE_ADMIN";
   const isKorisnik = decoded?.role?.authority === "ROLE_KORISNIK";
   //========================== OBJEKAT PRETRAGE ==================================
-  var dates = {
-    start: new Date(),
-    end: new Date(),
-  };
+
 
   // ========================== STATE ============================================
   const [tabela, setTabela] = useState([]);
-  const [params, setParams] = useState(dates);
+
 
   const [startDate, setStartDate] = useState(null);
   const [endDate, setEndDate] = useState(null);
   // /////////////////////////////////////////////////////// J A V A  S C R I P T  F U N K C I J E \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
   //======================== USE EFFECT ============================================
 
-  const getZadaci = useCallback(() => {
-    console.log("---------: ");
-    console.log("s:" + startDate + " E:" + endDate);
 
-    // console.log(params.start)
-    // console.log(params.end)
-    TestAxios.get("/report", {
-      // params:{
-      //     start: params.start,
-      //     end: params.end
-      // }
-
-      params: {
-        start: startDate,
-        end: endDate,
-      },
-    })
-      .then((res) => {
-        console.log(res);
-        setTabela(res.data);
-      })
-      .catch((error) => {
-        console.log(error);
-        alert("Error occured please try again!");
-      });
-  }, []);
 
   //======================== NAVIGATE ============================================
   var navigate = useNavigate();
 
-  const goToAdd = () => {
-    navigate("/new-movie");
-  };
 
   //============================================ HANDLERI ZA FORME I VALUE INPUT CHANGED ===============================
 
-  const valueInputChanged = (e) => {
-    const { name, value } = e.target;
-    console.log(name, value);
-    setParams((prevState) => ({
-      ...prevState,
-      [name]: value,
-    }));
-  };
-  const handleDateChange = (name, value) => {
-    setParams((prevState) => ({
-      ...prevState,
-      [name]: new Date(value), // Convert the value to a Date object
-    }));
-  };
 
-  {
-    /* ================================================ RENDER TABELE ========================================= */
-  }
+
+  { /* ================================================ RENDER TABELE ========================================= */ }
   //=============================================================================================================
   const renderTabela = () => {
     return tabela.map((klasa, index) => {
@@ -130,74 +84,38 @@ const AdminReport = () => {
   return (
     <div>
       <h1>Report</h1>
-      <div  style={{marginBottom: '50px'}}>
+      <div style={{ marginBottom: '50px' }}>
         <Row>
           <p>
             Please select dates, in order to generate Report for that period.
           </p>
         </Row>
-        {/* <Row>
-                    <Col md={2}>
-                    <FormGroup>
-                        <FormLabel htmlFor='start'>Start date</FormLabel>
-                        <Form.Control type='date' id='start' name='start' onChange={valueInputChanged}></Form.Control>
-                      </FormGroup>
-                      </Col>
-                      <Col md={2}>
-                      <FormGroup>
-                        <FormLabel htmlFor='end'>End date</FormLabel>
-                        <Form.Control type='date' id='end' name='end' onChange={valueInputChanged}></Form.Control>
-                      </FormGroup>
-                      </Col>
-                      <Col>
-                      <Button type='button' className='btn btn-warning' style={{marginTop: '30px'}} onClick={getZadaci}>Generate</Button>
-                      </Col>
-                </Row> */}
+
         <form onSubmit={handleSubmit}>
           <Row>
-            {/* <Col>
-              <input
-                type="date"
-                name="start"
-                onChange={(e) => {
-                  console.log(e.target.value);
-                  setStartDate(e.target.value);
-                }}
-              />
-            </Col>
-            <Col>
-              <input
-                type="date"
-                name="end"
-                onChange={(e) => {
-                  console.log(e.target.value);
-                  setEndDate(e.target.value);
-                }}
-              />
-            </Col> */}
 
-                    <Col md={2}>
-                    <FormGroup>
-                        <FormLabel htmlFor='start'>Start date</FormLabel>
-                        <Form.Control type='date' id='start' name='start'  onChange={(e) => {
-                  console.log(e.target.value);
-                  setStartDate(e.target.value);
+
+            <Col md={2}>
+              <FormGroup>
+                <FormLabel htmlFor='start'>Start date</FormLabel>
+                <Form.Control type='date' id='start' name='start' onChange={(e) => {
+                  console.log(e.target.value); setStartDate(e.target.value);
                 }}></Form.Control>
-                      </FormGroup>
-                      </Col>
-                      <Col md={2}>
-                      <FormGroup>
-                        <FormLabel htmlFor='end'>End date</FormLabel>
-                        <Form.Control type='date' id='end' name='end' onChange={(e) => {console.log(e.target.value); setEndDate(e.target.value)}}></Form.Control>
-                      </FormGroup>
-                      </Col>
+              </FormGroup>
+            </Col>
+            <Col md={2}>
+              <FormGroup>
+                <FormLabel htmlFor='end'>End date</FormLabel>
+                <Form.Control type='date' id='end' name='end' onChange={(e) => { console.log(e.target.value); setEndDate(e.target.value) }}></Form.Control>
+              </FormGroup>
+            </Col>
 
 
             <Col>
               <Button
                 type="submit"
                 className="btn btn-warning"
-                style={{marginTop: '33px'}}
+                style={{ marginTop: '33px' }}
               >
                 Generate
               </Button>
@@ -205,12 +123,12 @@ const AdminReport = () => {
           </Row>
         </form>
       </div>
-       
+
 
 
       <Row>
         <Col>
-        {tabela.length > 0 ? <h3>Report for period {formatOnlyDate(startDate)} - {formatOnlyDate (endDate)}</h3>  : ""}
+          {tabela.length > 0 ? <h3>Report for period {formatOnlyDate(startDate)} - {formatOnlyDate(endDate)}</h3> : ""}
           <Table id="movies-table">
             <thead>
               <tr>
